32501a8f121db5a524b7ea6261ededcfa953c9a1
chris <chiachun0920@gmail.com>
2019-07-28 02:33:14 +0000

    feat - add new order utility

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -2,28 +2,16 @@ import React from 'react';
 import classNames from 'classnames';
 import moment from 'moment';
 import { getAssetsPath } from '../utilities/assets';
+import { getProgress } from '../utilities/order';
 import * as ASSET_TYPE from '../constants/assetType';
 import * as ORDER_STATUS from '../constants/orderStatus';
 
 import './OrderItem.scss';
 
-const getProgress = status => {
-  switch (status) {
-    default:
-      throw new Error('error/invalid-status');
-    case ORDER_STATUS.CANCELED:
-    case ORDER_STATUS.DELIVERED:
-      return 'done';
-    case ORDER_STATUS.ESTABLISHED:
-    case ORDER_STATUS.PROCESSING:
-    case ORDER_STATUS.SHIPPED:
-      return 'inprogress';
-  }
-}
-
 const OrderItem = props => {
   const { item } = props;
-  const isInProgress = getProgress(item.status) === 'inprogress';
+
+  const isInProgress = getProgress(item.status) === ORDER_STATUS.INPROGRESS;
   const logo = isInProgress ? 'treemall' : 'treemall_dark';
 
   return (
--- src/constants/orderStatus.js
+++ src/constants/orderStatus.js
@@ -3,3 +3,7 @@ export const DELIVERED = '已送達';
 export const CANCELED = '已取消';
 export const ESTABLISHED = '已成立';
 export const PROCESSING = '處理中';
+
+export const INPROGRESS = '進行中';
+export const DONE = '已完成';
+
