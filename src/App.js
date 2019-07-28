d5cd10dde47d39c632c2ec0b07f5f2a33cf7bd67
chris <chiachun0920@gmail.com>
2019-07-28 02:52:15 +0000

    feat - categorize orders by status

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Panel, OrderList } from './components';
 import * as ORDER from './constants/order';
+import { getProgress } from './utilities/order';
 import mockOrders from './mocks/orders.json';
 
 function App() {
@@ -10,10 +11,15 @@ function App() {
     setOrders(mockOrders.orders);
   }, []);
 
+  const inProgressOrders = orders.filter(item => getProgress(item.status) === ORDER.INPROGRESS);
+  const doneOrders = orders.filter(item => getProgress(item.status) === ORDER.DONE);
   return (
     <div className="App">
       <Panel title={ORDER.INPROGRESS}>
-        <OrderList list={orders} />
+        <OrderList list={inProgressOrders} />
+      </Panel>
+      <Panel title={ORDER.DONE}>
+        <OrderList list={doneOrders} />
       </Panel>
     </div>
   );
